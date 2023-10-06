3b4a20b1b7888608119dc21f8e6f894f817db2d1
Nesma <nesmabdelazim@gmail.com>
2023-10-06 12:56:26 +0300

    Adding more features

---
+++ README.md
@@ -0,0 +1,26 @@
+# To-Do List App
+
+This is a to-do list app that helps you keep track of your tasks and get things done. You can add, delete tasks, and mark them as completed.
+
+## Features
+- Add new tasks
+- Delete tasks
+- Mark tasks as completed
+- Your tasks will be saved in the list untill you delete them.
+
+## Technologies Used
+- HTML
+- CSS
+- JavaScript
+
+## Installation
+1. Clone the repository to your local machine.
+2. Open the index.html file in your web browser.
+3. A link to the live version of the To-Do List App will be provided once it's ready.
+
+## Usage
+1. Open the app in your web browser.
+2. Fill in the task details in the input field.
+3. Click "Add" to add the task to your list.
+4. To delete a task, click on the "x" button next to the task name.
+5. To mark a task as completed, click on the task or the checkbox next to the task name.
--- source/main.js
+++ source/main.js
@@ -1,20 +1,44 @@
+
 let input = document.getElementById("input");
 let list = document.getElementById("list");
 
 function addTask(){
-    if (input.value !== ''){
+    if (input.value.trim() !== ''){
         let li = document.createElement("li");
         li.textContent = input.value;
 
-        list.appendChild(li);
         let del = document.createElement("span");
         del.textContent = "\u00d7";
-
         li.appendChild(del); 
+        list.appendChild(li);
     }
     
     else{
         alert("Can't Add Empty Task!");
     }
     input.value =""; 
+    storeList();
 }
+
+list.addEventListener('click', e => {
+    if (e.target.tagName === 'LI') {
+        e.target.classList.toggle('checked');
+        storeList()
+    }
+    else if(e.target.tagName === 'SPAN'){
+        e.target.parentElement.remove();
+        storeList()
+    }
+});
+
+function storeList(){
+    localStorage.setItem('data', list.innerHTML);
+}
+
+function showList(){
+    list.innerHTML = localStorage.getItem('data');
+} 
+
+showList();
+
+// localStorage.clear();
